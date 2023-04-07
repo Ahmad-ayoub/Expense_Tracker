8c2e33aa8fbb4146dd686096007ca2f2cb2f3d20
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-04-06 22:58:56 -0400

    new updates in JS

---
+++ script.js
@@ -0,0 +1,50 @@
+let type_array = [];
+let name_array = [];
+let date_array = [];
+let amount_array = [];
+
+const currencyTypeSelect = document.getElementById("currencyType");
+const nameSelect = document.getElementById("name");
+const dateSelect = document.getElementById("date");
+const amountSelect = document.getElementById("amount");
+const tablePlaceholder = document.getElementById("tablePlaceholder");
+const expensesRow = document.getElementById("expensesRow");
+
+function calculateExpenses() {
+  extractExpenses();
+  inputExpenses();
+  removePlaceholder();
+  appendExpenses();
+}
+
+function extractExpenses() {
+  currencyTypeSelect, nameSelect, dateSelect, amountSelect;
+  inputExpenses(currencyTypeSelect, nameSelect, dateSelect, amountSelect);
+}
+
+function inputExpenses(
+  currencyTypeSelect,
+  nameSelect,
+  dateSelect,
+  amountSelect
+) {
+  type_array.shift(currencyTypeSelect);
+  name_array.shift(nameSelect);
+  date_array.shift(dateSelect);
+  amount_array.shift(amountSelect);
+}
+
+function removePlaceholder(tablePlaceholder) {
+  tablePlaceholder.remove();
+}
+
+function appendExpenses(
+  currencyTypeSelect,
+  nameSelect,
+  dateSelect,
+  amountSelect,
+  expensesRow
+) {
+  const createExpensesRow = document.createElement("td");
+  expensesRow.appendChild(createExpensesRow);
+}
