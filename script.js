16de85c622ea508bcea5a11cffccb66e3cad6282
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-04-09 14:10:52 -0400

    final updates with comments

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
-let type_array = [];
-let name_array = [];
-let date_array = [];
-let amount_array = [];
+// A list of empty arrays to add the user data if needed for future functions or data collection
+let typeArray = [];
+let nameArray = [];
+let dateArray = [];
+let amountArray = [];
 
+// A list of global variables that will be used in the code to collect information from the HTML file
 const currencyTypeSelect = document.getElementById("currencyType");
 const nameSelect = document.getElementById("name");
 const dateSelect = document.getElementById("date");
 const amountSelect = document.getElementById("amount");
-const tablePlaceholder = document.getElementById("tablePlaceholder");
-const expensesRow = document.getElementById("expensesRow");
+let tablePlaceholder = document.getElementById("tablePlaceholder");
+const getExpenses = document.getElementById("calculateExpenses");
 
+// The starter function that begins the whole process
+getExpenses.addEventListener("click", calculateExpenses);
+
+// the main function that contains the functions that need to move in line
 function calculateExpenses() {
   extractExpenses();
-  inputExpenses();
   removePlaceholder();
-  appendExpenses();
 }
-
+// extracts all the user information and uses dot notation and arguments so that they can be utilized as a parameter in future functions.
 function extractExpenses() {
-  currencyTypeSelect, nameSelect, dateSelect, amountSelect;
-  inputExpenses(currencyTypeSelect, nameSelect, dateSelect, amountSelect);
+  const currencyTypeValue = currencyTypeSelect.value;
+  const nameValue = nameSelect.value;
+  const dateValue = dateSelect.value;
+  const amountValue = amountSelect.value;
+  inputExpenses(currencyTypeValue, nameValue, dateValue, amountValue);
+  appendExpenses([currencyTypeValue, nameValue, dateValue, amountValue]);
 }
-
-function inputExpenses(
-  currencyTypeSelect,
-  nameSelect,
-  dateSelect,
-  amountSelect
-) {
-  type_array.shift(currencyTypeSelect);
-  name_array.shift(nameSelect);
-  date_array.shift(dateSelect);
-  amount_array.shift(amountSelect);
+// adds in all the user information into the empty arrays above
+function inputExpenses(currencyTypeValue, nameValue, dateValue, amountValue) {
+  typeArray.push(currencyTypeValue);
+  nameArray.push(nameValue);
+  dateArray.push(dateValue);
+  amountArray.push(amountValue);
 }
-
-function removePlaceholder(tablePlaceholder) {
+// removes the placeholder held to explain to the user where the information will go
+function removePlaceholder() {
   tablePlaceholder.remove();
 }
+// adds in a new row and columns to the table div via simple JS methods in HTML with all the user information
+// the forEach loop is used so that this function can be used contionusly to add as much information as needed.
+function appendExpenses(values) {
+  let newRow = document.createElement("tr");
 
-function appendExpenses([currencyTypeSelect, nameSelect, dateSelect, amountSelect], expensesRow);
-{
-  const newRow = document.createElement("tr");
-
-  values.forEach(value => {
+  values.forEach((value) => {
     const newCell = document.createElement("td");
     newCell.textContent = value;
     newRow.appendChild(newCell);
   });
 
-  expensesRow.appendChild(newRow);
+  const tbody = document.querySelector(".adjustCells");
+  tbody.appendChild(newRow);
 }
-
